d33ad75b0f302b5fc5afc84777d239867252844b
Esfani05 <2021554564@estudiantec.cr>
2023-08-17 22:40:06 -0600

    tarea-2

---
+++ tareas/tarea-2/weather-app/src/main/java/tec/bd/weather/cli/MainCommand.java
@@ -0,0 +1,42 @@
+package tec.bd.weather.cli;
+
+import picocli.CommandLine;
+
+@CommandLine.Command(
+        name = "Weather App",
+        subcommands = {
+                ForecastByCityCommand.class,
+                ForecastByZipCodeCommand.class,
+                CreateForecastCommand.class,
+                UpdateForecastCommand.class,
+                RemoveForecastCommand.class,
+                CommandLine.HelpCommand.class
+        },
+        description = "Weather App")
+public class MainCommand implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
